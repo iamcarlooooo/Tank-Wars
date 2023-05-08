374b97dbd96856034333f328e390297824e59644
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-05-09 01:38:33 +0800

    Establishes moving towards and backwards of EnemyTank from player

--- Assets/EnemyTank.cs
+++ Assets/EnemyTank.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyTank : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    public Transform player;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } 
         
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+            
     }
 }
